add040a1cb905b83e84b2a57beecf49be006e0ae
Gary Bisson <gary.bisson@boundarydevices.com>
2016-06-08 08:59:09 +0200

    SystemUI: NavigationBarView: force rotation to 0
    Since the system takes care of every part of the rotation, the View
    must stay the one from a 0 degree perspective.
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -537,7 +537,7 @@ public class NavigationBarView extends LinearLayout {
     }
 
     private void updateCurrentView() {
-        final int rot = mDisplay.getRotation();
+        final int rot = Surface.ROTATION_0;
         for (int i=0; i<4; i++) {
             mRotatedViews[i].setVisibility(View.GONE);
         }
